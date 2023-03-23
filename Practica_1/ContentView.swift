23ebc665fe2b9d600d9cb700379e3f9eaccf049d
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-03-23 13:35:44 -0600

    Initial Commit

---
+++ Practica_1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Practica_1
+//
+//  Created by CEDAM20 on 23/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practica_1/Practica_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practica_1App.swift
+//  Practica_1
+//
+//  Created by CEDAM20 on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Practica_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
